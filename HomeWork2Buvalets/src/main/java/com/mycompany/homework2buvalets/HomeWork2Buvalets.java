f58441e01a8027d29746af4cbbebf75c74c50a19
DanilaBuvalets <58213927+DanilaBuvalets@users.noreply.github.com>
2023-02-26 16:15:44 +0300

    Add files via upload

---
+++ HomeWork2Buvalets/src/main/java/com/mycompany/homework2buvalets/HomeWork2Buvalets.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.homework2buvalets;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danil
+ */
+public class HomeWork2Buvalets {
+
+    public static void main(String[] args) {
+        System.out.println("Вариант 5  РИБО-02-21  Бувалец Данила Александрович");
+        Store basicStore = new Store();
+        basicStore.setNameStore();
+        basicStore.setNameStreet();
+        basicStore.setAreaStore();
+        basicStore.setNumberOfFloorsOfStore();
+        System.out.println(basicStore.toString());
+        GameStore gameStore = new GameStore();
+        gameStore.getBestGame();
+        gameStore.getAveragePrice();
+        gameStore.getMinimumProfit();
+        gameStore.getQuantityGame();
+        System.out.println(gameStore.toString());
+    }
+}
